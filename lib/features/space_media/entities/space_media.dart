d6886a5b8552d4827d9bb60eb15c52a3590ee810
Rodrigo Lima <rodrigolima9657@gmail.com>
2024-01-22 17:37:42 -0300

    add entity and usecase

---
+++ lib/features/space_media/entities/space_media.dart
@@ -0,0 +1,18 @@
+import 'package:equatable/equatable.dart';
+
+class SpaceMedia extends Equatable {
+  final String description;
+  final String mediaType;
+  final String title;
+  final String mediaUrl;
+
+  const SpaceMedia({
+    required this.description,
+    required this.mediaType,
+    required this.title,
+    required this.mediaUrl,
+  });
+
+  @override
+  List<Object?> get props => [description, mediaType, title, mediaUrl];
+}
